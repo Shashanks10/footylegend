916259ac26bff23c5b02a995b970567089830d15
shashank <shashanksatishkumar10@gmail.com>
2025-07-25 12:00:46 +0530

    forth deployment for prod config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/footylegend/',
+  base: mode === "production" ? "/footylegend/" : "/",
   server: {
     host: "::",
     port: 8080,
